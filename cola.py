effa894a6e0d0fab4e18e4dc54a1ef3fa70275c7
Antonio Melo Magallhães <rhavitony@gmail.com>
2023-11-15 23:14:18 -0300

    login

--- cola.py
+++ cola.py
@@ -5,7 +5,7 @@ os.getcwd()
 gc = pygsheets.authorize(service_file=os.getcwd() +
                          "/sistemasuelopro_googleConsole.json")
 arquivo = gc.open_by_url(
-    'https://docs.google.com/spreadsheets/d/1v654Gt4hmzRPZmsd4W85yiLf2gSeBiKNsMjdnvC8W9A/')
+    'https://docs.google.com/spreadsheets/d/15Jyo4qMmVK0JTSB95__JaVJveAOflbS1qR0qNOucEgI/')
 aba = arquivo.worksheet_by_title('main')
 header = aba.get_row(1)[0]
 print(header)
