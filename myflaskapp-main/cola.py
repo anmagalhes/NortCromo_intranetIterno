cff957539347af0d459fe8d9f43828c6d446faed
Antonio Melo Magallhães <rhavitony@gmail.com>
2023-12-23 18:46:17 -0300

    Atualizado_23.12.2023

---
+++ myflaskapp-main/cola.py
@@ -0,0 +1,2 @@
+worksheet.append_table(data=data_to_append, start='A1', end=None, dimension='ROWS', overwrite=False)
+aba.append_table(values=[o_que_escrever], start="A1", end=None, dimensions='ROWS', overwrite=False)
